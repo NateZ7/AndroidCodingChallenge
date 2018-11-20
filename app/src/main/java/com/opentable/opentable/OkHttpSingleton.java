e6a6318bbc5f60882e06a048313f4b3a43349f31
Nativ Barak <natebarak@gmail.com>
2018-11-20 15:15:54 -0800

    Using okhttp3, created an okHttpSingleton class

---
+++ app/src/main/java/com/opentable/opentable/OkHttpSingleton.java
@@ -0,0 +1,49 @@
+package com.opentable.opentable;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Class to handle networking and HTTP requests
+ */
+public class OkHttpSingleton {
+
+    // region Static Members
+    private static OkHttpSingleton sInstance;
+    // endregion
+
+    // region Class Members
+    private OkHttpClient mClient;
+    // endregion
+
+    // region Private Access
+
+    /**
+     * Singlton C'tor
+     */
+    private OkHttpSingleton() {
+        mClient = new OkHttpClient.Builder()
+                .retryOnConnectionFailure(true)
+                .build();
+    }
+    // endregion
+
+    // region Public Access
+
+    /**
+     * Getting the instance of the singlton
+     * @return new instance for the first time, existing instance otherwise
+     */
+    public static OkHttpSingleton getInstance() {
+        if (sInstance == null) {
+            sInstance = new OkHttpSingleton();
+        }
+
+        return sInstance;
+    }
+
+    private OkHttpClient getClient() {
+        return mClient;
+    }
+    // endregion
+}
+
