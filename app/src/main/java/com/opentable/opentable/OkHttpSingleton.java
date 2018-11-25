0fac9c6e0ce8b455d9a873874ecca891c76aa318
Nativ Barak <natebarak@gmail.com>
2018-11-25 14:24:41 -0800

    created getJson function and a result interface

--- app/src/main/java/com/opentable/opentable/OkHttpSingleton.java
+++ app/src/main/java/com/opentable/opentable/OkHttpSingleton.java
@@ -1,6 +1,14 @@
 package com.opentable.opentable;
 
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Class to handle networking and HTTP requests
@@ -46,6 +54,44 @@ public class OkHttpSingleton {
         return sInstance;
     }
 
+    /**
+     * Function to execute GET request
+     * @param url - endpoint to query
+     * @param result - an interface to id the sender for the result
+     */
+    public void getJson(String url, final IResult result) {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        getClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "GET request failed " + e.getMessage());
+                result.getResult(e.getMessage(), false);
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    result.getResult(response.body().string(), false);
+                    throw new IOException("Unexpected code " + response);
+                }
+
+                result.getResult(response.body().string(), true);
+            }
+        });
+    }
+    // endregion
+
+    // region Interfaces
+    /**
+     * Interface for getting results of HTTP requests
+     */
+    public interface IResult{
+        void getResult(String result, boolean isRequestSucceeded);
+    }
     // endregion
 }
 
